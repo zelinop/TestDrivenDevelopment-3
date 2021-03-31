189d01cb3c44ab4fb9f02c5decd155f5be2fc08d
unknown <zaeempetersen@gmail.com>
2021-03-31 21:30:31 +0200

    added programTest

--- src/main/java/za/ac/cput/testdrivendevelopment/program.java
+++ src/main/java/za/ac/cput/testdrivendevelopment/program.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package za.ac.cput.testdrivendevelopment;
 
 /**
  *
- * @author Zaeem
+ * @author Zaeem Petersen - 219010145
  */
 public class program {
-    
+
+    private int size;
+    private int version;
+
+    public program() {
+    }
+
+    @Override
+    public String toString() {
+        return "program{" + "size=" + size + ", version=" + version + '}';
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
 }
